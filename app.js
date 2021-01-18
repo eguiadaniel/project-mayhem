c7e09f8d589625da5a0babc07f67ceaaa71fb499
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-01-18 20:55:57 +0100

    Southpark GIF added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.render('home');
 });
 
 module.exports = app;
